52384c00f6d2683c003cc5e61235e602e906cf9c
sang <quocsangip@gmail.com>
2023-11-02 10:56:49 +0700

    add-data-route

--- api/router.js
+++ api/router.js
@@ -5,4 +5,14 @@ router.get('/check-health', (req, res) => {
     res.sendStatus(200)
 })
 
+router.post('/data', (req, res) => {
+    let gas = req.body.gas
+    let temperature = req.body.temperature
+    if (!gas || !temperature) res.sendStatus(400)
+    else {
+        console.log(gas, temperature)
+        res.sendStatus(200)
+    }
+})
+
 module.exports = router
